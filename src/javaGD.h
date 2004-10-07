a8b371bf8924f04ebbf7f947df031118329eb630
Simon Urbanek <simon.urbanek@R-project.org>
2004-10-07 14:39:52 +0000

    Bumped up version to 0.2 - supports get version
    git-svn-id: svn://svn.rforge.net/JavaGD/trunk@38 cd9d9a8b-87fc-4580-8bc7-ead42e10e920

--- src/javaGD.h
+++ src/javaGD.h
@@ -1,7 +1,7 @@
 #ifndef _DEV_JAVAGD_H
 #define _DEV_JAVAGD_H
 
-#define JAVAGD_VER 0x000107 /* JavaGD v0.1-7 */
+#define JAVAGD_VER 0x000200 /* JavaGD v0.2-0 */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
