f9cbf1997e7280197ff76b620c47ad7ed3f7e3a0
Simon Urbanek <simon.urbanek@R-project.org>
2005-12-28 23:11:34 +0000

    Correct version
    git-svn-id: svn://svn.rforge.net/JavaGD/trunk@44 cd9d9a8b-87fc-4580-8bc7-ead42e10e920

--- src/javaGD.h
+++ src/javaGD.h
@@ -1,7 +1,7 @@
 #ifndef _DEV_JAVAGD_H
 #define _DEV_JAVAGD_H
 
-#define JAVAGD_VER 0x000302 /* JavaGD v0.3-1 */
+#define JAVAGD_VER 0x000302 /* JavaGD v0.3-2 */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
