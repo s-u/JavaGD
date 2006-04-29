7564c51def7d9f7eb08bbec53f787d05139af8f0
Simon Urbanek <simon.urbanek@R-project.org>
2006-04-29 01:49:11 +0000

    Add .javaGD.get/set.class.path; fix warnings
    git-svn-id: svn://svn.rforge.net/JavaGD/trunk@50 cd9d9a8b-87fc-4580-8bc7-ead42e10e920

--- R/tools.R
+++ R/tools.R
@@ -1,6 +1,6 @@
 .javaGD.get.size <- function(devNr=dev.cur()) {
     par<-rep(0,6)
-    l<-.C("javaGDgetSize",as.integer(devNr-1),as.double(par))
+    l<-.C("javaGDgetSize",as.integer(devNr-1),as.double(par), PACKAGE="JavaGD")
     par<-l[[2]]
     if (par[6]==0) list() else list(x=par[1],y=par[2],width=(par[3]-par[1]),height=(par[4]-par[2]),dpiX=par[5],dpiY=par[6])
 }
@@ -16,10 +16,19 @@
 }
 
 .javaGD.version <- function() {
-	v<-.C("javaGDversion",as.integer(rep(0,4)))[[1]]
+	v<-.C("javaGDversion",as.integer(rep(0,4)), PACKAGE="JavaGD")[[1]]
 	list(major=v[1]%/%65536, minor=(v[1]%/%256)%%256, patch=(v[1]%%256), numeric=v[1])
 }
 
 .javaGD.set.display.parameters <- function(dpiX=100, dpiY=100, aspect=1) {
-	invisible(.C("javaGDsetDisplayParam",as.double(c(dpiX, dpiY, aspect))))
+	invisible(.C("javaGDsetDisplayParam",as.double(c(dpiX, dpiY, aspect)),PACKAGE="JavaGD"))
+}
+
+.javaGD.set.class.path <- function(cp) {
+  if (length(cp)<1) error("Invalid class path")
+  invisible(.C("setJavaGDClassPath",as.character(cp),PACKAGE="JavaGD"))
+}
+
+.javaGD.get.class.path <- function() {
+  .C("getJavaGDClassPath","",PACKAGE="JavaGD")[[1]]
 }
