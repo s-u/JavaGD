16452133bbe98db6b4f3d75d3b72f4a3ebb2fb30
Simon Urbanek <simon.urbanek@R-project.org>
2006-04-29 01:52:04 +0000

    Try to use rJava if it's loaded - it knows how to merge class paths
    git-svn-id: svn://svn.rforge.net/JavaGD/trunk@54 cd9d9a8b-87fc-4580-8bc7-ead42e10e920

--- R/JavaGD.R
+++ R/JavaGD.R
@@ -1,5 +1,11 @@
-JavaGD <- function(name="JavaGD", w=400, h=300, ps=12) {
-  invisible(.C("newJavaGD",as.character(name),as.numeric(w),as.numeric(h),as.numeric(ps),PACKAGE="JavaGD"))
+JavaGD <- function(name="JavaGD", width=400, height=300, ps=12) {
+  # if rJava is loaded, we use it to initialize the class path,
+  # because it knows how to merge class path into a running VM - we don't
+  if ("rJava" %in% .packages() && nchar(.javaGD.get.class.path())>0) {
+    .jinit(.javaGD.get.class.path())
+    .javaGD.set.class.path("")
+  }
+  invisible(.C("newJavaGD",as.character(name),as.numeric(width),as.numeric(height),as.numeric(ps),PACKAGE="JavaGD"))
 }
 
 .getJavaGDObject <- function(devNr) {
