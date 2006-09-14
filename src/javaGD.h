46373f3e0c6bacd85cb51cdceaa4ecb6a12a9da3
Simon Urbanek <simon.urbanek@R-project.org>
2006-09-14 17:53:58 +0000

    0.3-5 (fix compatibility with Java 1.6)
    git-svn-id: svn://svn.rforge.net/JavaGD/trunk@60 cd9d9a8b-87fc-4580-8bc7-ead42e10e920

--- src/javaGD.h
+++ src/javaGD.h
@@ -1,7 +1,7 @@
 #ifndef _DEV_JAVAGD_H
 #define _DEV_JAVAGD_H
 
-#define JAVAGD_VER 0x000304 /* JavaGD v0.3-4 */
+#define JAVAGD_VER 0x000305 /* JavaGD v0.3-5 */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
