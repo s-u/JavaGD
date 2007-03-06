446e12e24536dc28c81f9bc278b493a06d080f80
Simon Urbanek <simon.urbanek@R-project.org>
2007-03-06 17:39:33 +0000

    JavaGD 0.3-6 release
    git-svn-id: svn://svn.rforge.net/JavaGD/trunk@64 cd9d9a8b-87fc-4580-8bc7-ead42e10e920

--- src/javaGD.h
+++ src/javaGD.h
@@ -1,7 +1,7 @@
 #ifndef _DEV_JAVAGD_H
 #define _DEV_JAVAGD_H
 
-#define JAVAGD_VER 0x000305 /* JavaGD v0.3-5 */
+#define JAVAGD_VER 0x000306 /* JavaGD v0.3-6 */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
