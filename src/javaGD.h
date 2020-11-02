68b4662ef7f875fb334db34b50219770c1112f6d
Simon Urbanek <simon.urbanek@R-project.org>
2020-11-02 15:44:06 +1300

    minor packaging changes to keep CRAN happy

--- src/javaGD.h
+++ src/javaGD.h
@@ -1,7 +1,7 @@
 #ifndef _DEV_JAVAGD_H
 #define _DEV_JAVAGD_H
 
-#define JAVAGD_VER 0x000603 /* JavaGD v0.6-3 */
+#define JAVAGD_VER 0x000604 /* JavaGD v0.6-4 */
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
